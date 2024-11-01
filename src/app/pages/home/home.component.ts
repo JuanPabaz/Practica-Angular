087b7202e0a0d3651e63203d0b4b55556b2da7c0
JuanPabaz <jpgthf@gmail.com>
2024-11-01 10:28:09 -0500

    feat: add changeHandler and deleteTask

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,4 +12,14 @@ export class HomeComponent {
   tasks = signal(['Instalar el Angular CLI',
     'Crear proyecto',
     'Crear componentes',]);
+
+    changeHandler(event: Event){
+      const input = event.target as HTMLInputElement;
+      const newTask = input.value;
+      this.tasks.update((tasks) => [...tasks, newTask]);
+    }
+
+    deleteTask(index: number){
+      this.tasks.update((tasks) => tasks.filter((tasks,i) => i !== index));
+    }
 }
